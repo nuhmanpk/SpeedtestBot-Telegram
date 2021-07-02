3c25ac30ec66a0b9ebf386aa04b3f7ae3107577c
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-07-02 11:41:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 
 import os 
 from os import error
+import speedtest   
 import logging
 import pyrogram
 from decouple import config
@@ -14,11 +15,21 @@ from pyrogram.types import User, Message
 
     
 bughunter0 = Client(
-    "Sticker-Bot",
+    "SpeedTestBot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
 
+
 @bughunter0.on_message(filters.command(["start"]))
 async def start(bot, update):
+ txt = await update.reply_text("ചത്തോന്ന് അറിയാൻ വന്നതാവും ല്ലേ !!")
+
+@bughunter0.on_message(filters.private)
+async def download(bot, message):
+     downloadSpeed = speedtest.Speedtest() 
+     txt = await message.reply_text(f"{downloadSpeed.download()} Mbps")
+     txt1 = await message.reply_text(f"{downloadSpeed.upload} Mbps")
+
+bughunter0.run()
